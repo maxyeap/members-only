2c6df00c36f15b55bc509d7aa6f71eecaa16fc31
Max Yeap <max@yongtuck.com>
2023-05-02 22:37:49 +0800

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+
+  def index
+    @posts = Post.all.order(created_at: :asc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to action: 'index'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :users, only: %i[new create index]
+  root 'posts#index'
+  resources :posts, only: %i[new create index]
 end
